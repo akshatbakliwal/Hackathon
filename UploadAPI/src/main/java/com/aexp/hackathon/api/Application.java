9a2a258aeae0e4f4083f5d85d3f855b6784b378a
Akshat Bakliwal <akshat.bakliwal@aexp.com>
2015-07-23 19:58:20 +0530

    Enhancements

--- UploadAPI/src/main/java/com/aexp/hackathon/api/Application.java
+++ UploadAPI/src/main/java/com/aexp/hackathon/api/Application.java
@@ -1,5 +1,7 @@
 package com.aexp.hackathon.api;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,14 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+		String base_path = System.getProperty("user.dir");
+		File file = new File(base_path+"/uploadedJars/");
+		if (!file.exists()) {
+			file.mkdir();
+		}
+		file = new File(base_path+"/Documentation/");
+		if (!file.exists()) {
+			file.mkdir();
+		}
     }
 }
